04a9055ed3d54ea3ccdd415f7bc76c9b22096ed6
gabibrevi <gabibrevi@gmail.com>
2024-08-26 06:26:37 +0200

    Tailwindcss postcss autoprefixer intalled. React-router-dom intalled. Linking layout and router

---
+++ src/layouts/AppLayout.tsx
@@ -0,0 +1,24 @@
+import { Outlet } from 'react-router-dom'
+import Logo from '../components/logo'
+
+export default function AppLayout() {
+    return (
+        <>
+            <header className=' bg-gray-800 py-5'>
+                <div className=' max-w-screen-2xl mx-auto flex flex-col lg:flex-row justify-between items-center'>
+                    <div className=' w-64'>
+                        <Logo />
+                    </div>
+                </div>
+            </header>
+
+            <section className=' max-w-screen-2xl mx-auto mt-10 p-5'>
+                <Outlet />
+            </section>
+
+            <footer className=' py-5 '>
+                <p className='text-center'> All right reserved {new Date().getFullYear()}</p>
+            </footer>
+        </>
+    )
+}
